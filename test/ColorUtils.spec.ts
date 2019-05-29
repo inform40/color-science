a9d37966ab57eab807e63837b4dbb95b37e44626
Oliver Conzen <oliver.conzen@inform-software.com>
2019-05-29 09:43:58 +0200

    Adds test for rgbToHex

--- test/ColorUtils.spec.ts
+++ test/ColorUtils.spec.ts
@@ -4,7 +4,7 @@ import {ColorUtils} from "../src/ColorUtils";
 
 describe("ColorUtils", () => {
     describe("getColorCodeType", () => {
-        it("should correctly detect Hex3", () => {
+        it("should detect Hex3", () => {
             expect(ColorUtils.getColorCodeType("#000")).to.equal(ColorCode.Hex3);
             expect(ColorUtils.getColorCodeType("#fff")).to.equal(ColorCode.Hex3);
             expect(ColorUtils.getColorCodeType("#FFF")).to.equal(ColorCode.Hex3);
@@ -18,7 +18,7 @@ describe("ColorUtils", () => {
                 ColorUtils.getColorCodeType("000");
             }).to.throw;
         });
-        it("should correctly detect Hex6", () => {
+        it("should detect Hex6", () => {
             expect(ColorUtils.getColorCodeType("#000000")).to.equal(ColorCode.Hex6);
             expect(ColorUtils.getColorCodeType("#ffffff")).to.equal(ColorCode.Hex6);
             expect(ColorUtils.getColorCodeType("#FFFFFF")).to.equal(ColorCode.Hex6);
@@ -32,7 +32,7 @@ describe("ColorUtils", () => {
                 ColorUtils.getColorCodeType("000000");
             }).to.throw;
         });
-        it("should correctly detect Rgb", () => {
+        it("should detect Rgb", () => {
             expect(ColorUtils.getColorCodeType("rgb(0,0,0)")).to.equal(ColorCode.Rgb);
             expect(ColorUtils.getColorCodeType("rgb(255,255,255)")).to.equal(ColorCode.Rgb);
             expect(ColorUtils.getColorCodeType("rgb(0 , 0 , 0)")).to.equal(ColorCode.Rgb);
@@ -57,4 +57,30 @@ describe("ColorUtils", () => {
         });
         */
     });
+    describe("rgbToHex", () => {
+        it("should throw on invalid input", () => {
+            expect(() => {
+                ColorUtils.rgbToHex("");
+            }).to.throw;
+            expect(() => {
+                ColorUtils.rgbToHex("#000");
+            }).to.throw;
+            expect(() => {
+                ColorUtils.rgbToHex("#000000");
+            }).to.throw;
+            expect(() => {
+                ColorUtils.rgbToHex("rgb(0,0,0");
+            }).to.throw;
+        });
+        it("should convert rgb", () => {
+            expect(ColorUtils.rgbToHex("rgb(0,0,0)")).to.equal("#000000");
+            expect(ColorUtils.rgbToHex("rgb(0 , 0 , 0)")).to.equal("#000000");
+            expect(ColorUtils.rgbToHex("rgb(255,255,255)")).to.equal("#ffffff");
+            expect(ColorUtils.rgbToHex("rgb(42 , 148 , 240)")).to.equal("#2a94f0");
+        });
+    });
+
+    describe("rgbaToHex", () => {
+        
+    })
 });
